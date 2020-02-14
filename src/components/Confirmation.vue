b3392d5355a14f71079b466e8a30af5a73b1e548
6aldej <49287487+6aldej@users.noreply.github.com>
2020-02-14 22:01:13 +0300

    Add files via upload

---
+++ src/components/Confirmation.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <b-alert variant="success" show>{{message}}
+        <button
+          v-on:click="showConfirmation = false"
+          type="button" class="close" data-dismiss="alert"
+          aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </b-alert>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['message'],
+};
+</script>
